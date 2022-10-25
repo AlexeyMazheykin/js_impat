d405d61b8e9ed17489db1ca45404d94f6a7f727f
Alexey Mazhekin <mazheykin@icloud.com>
2022-10-26 00:04:40 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+
 console.log("hhh")
+const notQuitePi = parseInt('1.9')
+console.log(notQuitePi)
+console.log(typeof NaN)
+console.log("j"+undefined)
+console.log(+[])
+console.log([1,2,3,"huy vam v rot"].toString())
+
+console.log(Boolean(7))
+//console.log(this)
+function aaa () {
+    console.log(this)
+}
+//aaa()
+console.log(module.exports)
